300726ed09791501e511e296914b157b5f386c5b
Nikhil Gautam <nikhilgautam303@gmail.com>
2025-08-14 19:12:00 +0530

    Fixes PORT variable

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ sequelize
   .then(() => {
     console.log("Database connected & synced");
     app.listen(process.env.PORT, "0.0.0.0", () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on port ${process.env.PORT}`);
     });
   })
   .catch((err) => {
