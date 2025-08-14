d6b1329c68982089b6becda765f5bddee2d06047
Nikhil Gautam <nikhilgautam303@gmail.com>
2025-08-14 18:44:29 +0530

    Changes server.js

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ sequelize
   .sync({ alter: true })
   .then(() => {
     console.log("Database connected & synced");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port: ${process.env.PORT}`);
+    app.listen(process.env.PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
